6c30224fe861db0f1ff07a7c54b07a5f1cb75f63
taisazero <ealhossa@uncc.edu>
2023-02-17 21:22:13 +0000

    fixed argument had file2path as required by mistake

--- pile/final_vetting/read_json.py
+++ pile/final_vetting/read_json.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     key is an optional argument. If not provided, it will try to read the key from the environment variable S3_KEY.
     """
     def run(file1_path: str = typer.Argument(..., help="The name and path of the first file in the S3 bucket."),
-            file2_path: str = typer.Option(..., help="The name and path of the second file in the S3 bucket."),
+            file2_path: str = typer.Option(None, help="The name and path of the second file in the S3 bucket."),
             key: str = typer.Option(None, help="The key of the JSONL file in the S3 bucket. If None, it will try to read the key from the environment variable S3_KEY."),
             sample_size: int = typer.Option(None, help="The number of lines to read from the JSONL file. If None, it will read the entire file."),
             start_index: int = typer.Option(0, help="The index of the first line to read from the JSONL file.")):
@@ -66,20 +66,21 @@ if __name__ == "__main__":
         file1_path = file1_path.replace("s3://", "")
         if file2_path is not None:
             file2_path = file2_path.replace("s3://", "")
-            file2_contents = read_jsonl_from_s3(file2_path, key, sample_size=sample_size, start_index=start_index)
+            file2_contents = [decode_sequence(seq) for seq in read_jsonl_from_s3(file2_path, key, sample_size=sample_size, start_index=start_index)]
         else:
             file2_contents = None
 
-        file1_contents = read_jsonl_from_s3(file1_path, key, sample_size=sample_size, start_index=start_index)
+        file1_contents = [decode_sequence(seq) for seq in read_jsonl_from_s3(file1_path, key, sample_size=sample_size, start_index=start_index)]
 
         exact_matches = 0
         if file2_contents is not None:
             for f1_line, f2_line in zip(file1_contents, file2_contents):
                 i = 0
                 # compare the two lines if equal then print one and say they match if they don't print both and say they don't match
+
                 if f1_line == f2_line:
                     print(f"Files match on line {i}")
-                    print(f1_line)
+                    print(decode_sequence(f1_line))
                     exact_matches += 1
                 else:
                     print(f"Files do not match on line {i}")
