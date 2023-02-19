bb7ae13f72eff30eb59225610c4a997d4da0fdab
taisazero <ealhossa@uncc.edu>
2023-02-19 23:14:19 +0000

    added pyspark processing of groups for tok

---
+++ pyspark/processing/pyspark_tokenize.py
@@ -0,0 +1,131 @@
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import RegexTokenizer, NGram, HashingTF, MinHashLSH
+from pyspark.sql.functions import col
+from spark_session_builder import build_spark_session
+import tiktoken
+import os
+import argparse
+from pathlib import Path
+import json
+import os
+import boto3
+
+from pyspark.sql.functions import udf
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--data_dir",
+    type=str,
+    required=True,
+    help="The directory where the data is stored.",
+)
+parser.add_argument(
+    "--output_dir",
+    type=str,
+    required=False,
+    help="The path where the output should be stored.",
+)
+
+parser.add_argument(
+    "--node",
+    type=str,
+    required=False,
+    default="cpu32-dy-m6i-8xlarge-1",
+    help="The node to connect to.",
+)
+
+# get num_cores and memory from args
+parser.add_argument(
+    "--num_cores",
+    type=int,
+    required=False,
+    default=16,
+    help="The number of cores to use.",
+)
+
+parser.add_argument(
+    "--memory",
+    type=int,
+    required=False,
+    default=32,
+    help="The amount of memory in GB to use.",
+)
+
+# get the cluster job id from args
+parser.add_argument(
+    "--cluster_job_id",
+    type=str,
+    required=False,
+    default="",
+    help="The cluster job id.",
+)
+
+
+
+args = parser.parse_args()
+
+spark = build_spark_session(f"spark://{args.node}:7077", args.num_cores, args.memory)
+
+def tokenize(text):
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    result = tokenizer.encode(text, disallowed_special=())
+    return json.dumps({"input_ids": result, "attention_mask": [1] * len(result)})
+
+data_dir = Path(args.data_dir)
+files = [str(f) for f in list(data_dir.glob("*.parquet"))]
+
+print(files)
+
+if args.output_dir is None:
+    root = f"/fsx/shared/pilev2/group3_filtered_decontam/the_stack_jsonl/{args.data_dir.split('/')[-1]}"
+else:
+    root = args.output_dir
+
+data = spark.read.parquet(*files)
+find_tokenize = udf(tokenize)
+
+data = data.withColumn("tokenized", find_tokenize(data["text"]))
+data = data.select("tokenized")
+
+# print("Number of files: ", num_files)
+# Write each partition to a separate JSONL file
+max_file_size = 250 * 1024 * 1024 # 250 MB
+(
+    data.write
+    .option("maxFileSize", max_file_size)
+    .format("json")
+    .mode("overwrite")
+    .text(
+        args.output_dir,
+        compression="gzip",
+        lineSep ="\n"
+    )
+)
+
+
+# Rename all the files to .jsonl in the output directory
+# This is because the spark jsonl writer adds a .txt.gz extension
+# Handle this if output_dir is in s3 using the aws cli and os.system
+if args.output_dir.startswith("s3a://"):
+    output_dir = args.output_dir.replace("s3a://", "")
+    # get all the files in the output directory
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(output_dir.split("/")[0])
+    bucket_name = output_dir.split("/")[0]
+    dataset_name = output_dir.split("/")[1]
+    files = [f"s3://{bucket_name}/{dataset_name}/{f.key}" for f in bucket.objects.filter(Prefix=output_dir.split("/")[2])]
+    for file in files:
+        if file.endswith(".txt.gz"):
+            print(f"aws s3 mv {file} {file.replace('.txt.gz', '.json.gz')}")
+            os.system(f"aws s3 mv {file} {file.replace('.txt.gz', '.json.gz')}")
+           
+else:
+    for file in os.listdir(args.output_dir):
+        if file.endswith(".txt.gz"):
+            os.rename(os.path.join(args.output_dir, file), os.path.join(args.output_dir, file.replace(".txt.gz", ".json.gz")))
+
+
+# Run scancel on the job id to terminate the cluster
+print(f"scancel {args.cluster_job_id}")
+os.system(f"scancel {args.cluster_job_id}")
---
+++ pyspark/processing/tokenize_group.sh
@@ -0,0 +1,127 @@
+#! /bin/bash
+
+# PySpark tokenization for group 2
+# Set variables
+DATA_PATH=/fsx/shared/pilev2/group3_filtered_decontam/group3_parquet
+# DO NOT include a trailing slash in the output path
+OUTPUT_PATH=s3a://stability-llm/jsonl_tiktoken_tests
+
+#TODO: Change me
+spark_path=/fsx/home-erfan/pilev2/pyspark
+
+# Set up logs directories
+mkdir -p $spark_path/logs
+mkdir -p $spark_path/processing/logs
+
+# Create a list all of the directories in $DATA_PATH
+# datasets=$(ls $DATA_PATH)
+datasets=("TED2020" "AI4Code_ver2")
+cpus=32
+mem=32
+partition=cpu32
+nodes=2
+pyspark_server=./pyspark_server.slurm
+
+# Loop through groups and submit slurm job for each dataset
+for dataset in ${datasets[@]}; do   
+    # list all files in $DATA_PATH/$dataset
+    temp_dir=$DATA_PATH/$dataset
+    basename_dataset=$(basename $dataset)
+    basename_dataset=${basename_dataset%.*}
+    echo $DATA_PATH/$dataset
+    cat << HELP > $pyspark_server
+#!/bin/bash
+# job name is the basename of the dataset and file
+#SBATCH --job-name=cls_$basename_dataset
+#SBATCH --output=./logs/$basename_dataset.o
+#SBATCH --error=./logs/$basename_dataset.e
+#SBATCH --nodes=$nodes
+#SBATCH --exclusive
+#SBATCH --mem=0
+#SBATCH --cpus-per-task=$cpus
+#SBATCH --partition=$partition
+#SBATCH --comment=carper
+#SBATCH --export=ALL
+# ===== END SLURM OPTIONS =====
+source /fsx/home-erfan/miniconda3/bin/activate pilev2
+cd $spark_path
+sleep 2
+# Write jobId to a file
+#echo $SLURM_JOB_ID > ./logs/$basename_dataset.jobid.txt
+# Write the hostnames of the nodes to a file
+srun --comment carper hostname > ./logs/$basename_dataset.hostnames.txt
+# Start the Spark cluster
+srun --comment carper bash worker_spark_on_slurm.sh
+HELP
+    # sbatch $pyspark_server 
+    # save the job id from the output of sbatch
+    job_id=$(sbatch $pyspark_server | cut -d ' ' -f 4)
+    echo $job_id
+    rm $pyspark_server
+    # Check if the cluster is up and running
+    # If not, wait 30 seconds and try again
+    while [ ! -f $spark_path/logs/$basename_dataset.hostnames.txt ]; do
+        sleep 30
+    done
+    # add job id to a file
+    echo $job_id >> $spark_path/logs/$basename_dataset.jobid.txt
+
+# submit job to tokenize the dataset
+# read the hostnames from the file
+hostnames=$(cat $spark_path/logs/$basename_dataset.hostnames.txt)
+# get the first hostname
+# loop through the hostnames and get the one with the lowest number
+#
+for hostname in $hostnames; do
+    # get the last element of the hostname
+    # this is the node number
+    node_number=$(echo $hostname | cut -d '-' -f 5)
+    # if the node number is less than the current min, update the min
+    if [ -z $min_node_number ]; then
+        min_node_number=$node_number
+        min_hostname=$hostname
+
+    elif [ $node_number -lt $min_node_number ]; then
+        min_node_number=$node_number
+        min_hostname=$hostname
+    fi
+done
+
+node_name=$min_hostname
+
+echo $node_name
+# get the job id as a string
+cluster_job_id=$(cat $spark_path/logs/$basename_dataset.jobid.txt)
+# get the first element of the job id
+cluster_job_id=$(echo $cluster_job_id | cut -d ' ' -f 1)
+echo $cluster_job_id
+worker_submission=./worker.slurm
+cat << HELP > $worker_submission
+#!/bin/bash
+# job name is the basename of the dataset and file
+#SBATCH --job-name=rn_$basename_dataset
+#SBATCH --output=./logs/rn_$basename_dataset.o
+#SBATCH --error=./logs/rn_$basename_dataset.e
+#SBATCH --exclusive
+#SBATCH --mem=4GB
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=cpu16
+#SBATCH --comment=carper
+#SBATCH --export=ALL
+# ===== END SLURM OPTIONS =====
+source /fsx/home-erfan/miniconda3/bin/activate pilev2
+cd $spark_path/processing
+python pyspark_tokenize.py \
+    --data_dir $DATA_PATH/$dataset \
+    --output_dir $OUTPUT_PATH/$dataset \
+    --node $node_name \
+    --num_cores $cpus \
+    --memory $mem \
+    --cluster_job_id $cluster_job_id 
+HELP
+    sbatch $worker_submission
+    rm $worker_submission
+    # pause for 2 seconds.
+    sleep 2
+done
+
